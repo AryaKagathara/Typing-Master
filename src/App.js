d45f9a4b17d1e4e78f26dcedc67158c8f55b0543
AryaKagathara <iamaryak27@gmail.com>
2020-09-23 09:13:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ import Speed from "./components/Speed";
 import StartButton from "./components/StartButton";
 import TextArea from "./components/TextArea";
 
-const list = [
-  "This work is subject to copyright. All rights are reserved by the Publisher, whether the whole or part of the material is concerned, specifically the rights of translation, reprinting, reuse of illustrations, recitation, broadcasting, reproduction on microfilms or in any other physical way, and transmission or information storage and retrieval, electronic adaptation, computer software, or by similar or dissimilar methodology now known or hereafter developed.",
-  "Most companies conduct their interviews in very similar ways. We will offer an overview of how companies interview and what they're looking for. This information should guide your interview preparation and your reactions during and after the interview.",
-  "Understanding the common sorting and searching algorithms is incredibly valuable, as many of sorting and searching problems are tweaks of the well-known algorithms. A good approach is therefore to run through the different sorting algorithms and see if one applies particularly well",
-  "GAYLE LAAKMANN MCDOWELL is the author of The Google Resume and the founder and CEO of CareerCup.com, which helps candidates prepare for technical interviews. She has worked at Microsoft, Google, and Apple as a software engineer, served on Google's hiring committee, and has interviewed over 150 software engineering candidates",
-  "As soon as we've allowed for a while loop, our work gets much easier. We just need to generate a range of values where each value is equally likely (and where the range has at least seven elements). If we can do this, then we can discard the elements greater than the previous multiple of 7, and mod the rest of them by 7. This will get us a value within the range of 0 to 6, with each value being equally likely.",
-  "Declaring the constructor private will ensure that no one outside of the class can directly instantiate the class. In this case, the only way to create an instance of the class is to provide a static public method, as is done when using the Factory Method Pattern.",
-  "Hello World",
-];
+import list from './List';
 
 class App extends Component {
   constructor(props) {
